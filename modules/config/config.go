b5166fe7197b2d5a84c4c2fbaeeec48bf9361960
luna-nightbyte <lunawifu18@gmail.com>
2025-02-20 23:17:36 +0100

    update config

--- modules/config/config.go
+++ modules/config/config.go
@@ -19,9 +19,9 @@ type youtubeDL struct {
 	Binary string `json:"binary"`
 }
 type app struct {
-	mux sync.Mutex
-
+	mux           sync.Mutex
 	Port          int        `json:"port"`
+	Loop_interval int        `json:"loop_interval_in_minutes"`
 	Videos_folder string     `json:"video_output_folder"`
 	RateLimit     rate_limit `json:"rate_limit"`
 	ExportPath    string     `json:"default_export_location"`
