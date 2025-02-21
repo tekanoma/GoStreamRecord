b678a1ef10e64f52343a3850eb8e2a96e0f1df86
luna-nightbyte <lunawifu18@gmail.com>
2025-02-21 22:38:04 +0100

    added default files for examples

--- modules/file/path.go
+++ modules/file/path.go
@@ -3,7 +3,7 @@ package file
 import "strings"
 
 var (
-	Config_path          string = "./internal/settings/app.json"
+	Config_path          string = "./internal/settings/settings.json"
 	YoutubeDL_configPath string = "./internal/settings/youtube-dl.config"
 	Index_path           string = "./internal/web/index.html"
 	Login_path           string = "./internal/web/login.html"
